2c6d8f62835835e7c9fa4d658c9f87491616cd68
Anna Nabokova <talfril@gmail.com>
2025-01-13 13:50:12 +0100

    m

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,6 @@ import ReactDOM from 'react-dom/client';
 
 import { Root } from '@/components/Root';
 
-import './mockEnv.ts';
-
-import '@telegram-apps/telegram-ui/dist/styles.css';
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(<Root/>);
